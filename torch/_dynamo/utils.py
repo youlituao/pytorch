youlituao/pytorch

commit 24dd6d26196324293ce1d158040395776e40a5ef

[Dynamo] Simplify DunderDictVariable impl. (#177658)
Pull Request resolved: https://github.com/pytorch/pytorch/pull/177658
Approved by: https://github.com/Lucaskabela
ghstack dependencies: #176773, #178185, #177659

torch/_dynamo/variables/dicts.py
@@ -38,7 +38,12 @@ from ..bytecode_transformation import (
 )
 from ..exc import raise_observed_exception, unimplemented
 from ..guards import GuardBuilder, install_guard
-from ..source import AttrSource, is_constant_source, is_from_local_source
+from ..source import (
+    AttrSource,
+    DictGetItemSource,
+    is_constant_source,
+    is_from_local_source,
+)
 from ..utils import (
     cmp_name_to_op_mapping,
     dict_items,
@@ -51,6 +56,7 @@ from ..utils import (
 from .base import (
     AttributeMutationExisting,
     AttributeMutationNew,
+    NO_SUCH_SUBOBJ,
     ValueMutationNew,
     VariableTracker,
 )
@@ -64,7 +70,6 @@ from .constant import (
 
 if TYPE_CHECKING:
     from torch._dynamo.codegen import PyCodegen
-    from torch._dynamo.side_effects import SideEffects
     from torch._dynamo.symbolic_convert import InstructionTranslator
     from torch._dynamo.variables.builtin import BuiltinVariable
 
@@ -74,10 +79,6 @@ if TYPE_CHECKING:
 # [Adding a new supported class within the keys of ConstDictVariable]
 # - Implement is_python_hashable() method in the VariableTracker subclass
 # - Implement get_python_hash() and is_python_equal() methods for hashable types
-
-
-def was_instancecheck_override(obj: Any) -> bool:
-    return type(obj).__dict__.get("__instancecheck__", False)
 
 
 def raise_unhashable(
@@ -1983,17 +1984,62 @@ class SideEffectsProxyDict(collections.abc.MutableMapping[kV, VariableTracker]):
     effects table as storage.
     """
 
-    def __init__(self, item: VariableTracker, side_effects: "SideEffects") -> None:
+    @staticmethod
+    def get_example_value_dict(vt: VariableTracker) -> dict[str, object]:
+        if istype(vt, variables.NestedUserFunctionVariable):
+            # NestedUserFunctionVariable is created with MAKE_FUNCTION and its
+            # __dict__ starts empty. Any mutation will actually be recorded in
+            # the side effects table
+            return {}
+        elif isinstance(vt, variables.LocalGeneratorFunctionVariable):
+            return SideEffectsProxyDict.get_example_value_dict(vt.vt)
+        else:
+            value = vt.get_real_python_backed_value()
+            if value is not NO_SUCH_SUBOBJ:
+                return object.__getattribute__(value, "__dict__")
+            else:
+                unimplemented(
+                    gb_type="unsupported variable type for __dict__ access",
+                    context=f"VariableTracker type: {type(vt)}",
+                    explanation=f"Dynamo does not know how to get __dict__ from {type(vt)}",
+                    hints=[
+                        *graph_break_hints.DYNAMO_BUG,
+                    ],
+                )
+
+    @staticmethod
+    def get_value___dict__(
+        tx: "InstructionTranslator", vt: VariableTracker
+    ) -> dict[str, VariableTracker]:
+        example_value_dict = SideEffectsProxyDict.get_example_value_dict(vt)
+
+        return {
+            key: VariableTracker.build(
+                tx,
+                value,
+                source=vt.source
+                and DictGetItemSource(AttrSource(vt.source, "__dict__"), key),
+            )
+            for key, value in example_value_dict.items()
+        }
+
+    def __init__(self, item: VariableTracker, tx: "InstructionTranslator") -> None:
         self.item = item
-        self.side_effects = side_effects
+        self.side_effects = tx.output.side_effects
+        self.item_dict = self.get_value___dict__(tx, item)
 
     def _maybe_unwrap_key(self, key: kV) -> str:
         Hasher = ConstDictVariable._HashableTracker
         return key.vt.as_python_constant() if istype(key, Hasher) else key
 
+    def side_effects_table(self) -> dict[str, VariableTracker]:
+        return self.side_effects.store_attr_mutations.get(self.item, {})
+
     def __getitem__(self, key: kV) -> VariableTracker:
         name = self._maybe_unwrap_key(key)
-        return self.side_effects.load_attr(self.item, name)
+        if self.side_effects.has_pending_mutation_of_attr(self.item, name):
+            return self.side_effects.load_attr(self.item, name, deleted_ok=True)
+        return self.item_dict[name]
 
     def __setitem__(self, key: kV, value: VariableTracker) -> None:
         # Find a way to not hash the key using _HashableTracker
@@ -2007,18 +2053,28 @@ class SideEffectsProxyDict(collections.abc.MutableMapping[kV, VariableTracker]):
 
     def __contains__(self, key: kV) -> bool:  # type: ignore[bad-override]
         name = self._maybe_unwrap_key(key)
-        return name in self.side_effects.store_attr_mutations.get(self.item, {})
+        table = self.side_effects_table()
+        # if name in side effects, then it is only contained if it's not a DeletedVariable
+        # even if the original dict contains it
+        if name in table:
+            return not isinstance(table[name], variables.DeletedVariable)
+        else:
+            return name in self.item_dict
 
     def __len__(self) -> int:
-        return len(self.side_effects.store_attr_mutations.get(self.item, {}))
+        return sum(1 for _ in self)
 
     def __iter__(self) -> Iterator[ConstDictVariable._HashableTracker]:
         Hasher = ConstDictVariable._HashableTracker
-        d = self.side_effects.store_attr_mutations.get(self.item, {})
+        d = self.side_effects_table()
         for k, v in d.items():
             if isinstance(v, variables.DeletedVariable):
                 continue
             yield Hasher(ConstantVariable.create(k))
+
+        for k, v in self.item_dict.items():
+            if k not in d:
+                yield Hasher(ConstantVariable.create(k))
 
 
 class DunderDictVariable(ConstDictVariable):
@@ -2029,14 +2085,13 @@ class DunderDictVariable(ConstDictVariable):
         cls,
         tx: "InstructionTranslator",
         vt: VariableTracker,
-        dict_proxy: dict[str, VariableTracker],
     ) -> "DunderDictVariable":
         mutation = AttributeMutationExisting() if vt.source else AttributeMutationNew()
         source = vt.source and AttrSource(vt.source, "__dict__")
+
         return cls(
             vt,
-            dict_proxy=types.MappingProxyType(dict_proxy),
-            side_effects=tx.output.side_effects,
+            tx=tx,
             mutation_type=mutation,
             source=source,
         )
@@ -2044,28 +2099,20 @@ class DunderDictVariable(ConstDictVariable):
     def __init__(
         self,
         vt: VariableTracker,
-        dict_proxy: types.MappingProxyType[str, VariableTracker],  # object __dict__
-        side_effects: "SideEffects",
+        tx: "InstructionTranslator",
         **kwargs: Any,
     ) -> None:
         super().__init__({}, **kwargs)
-        self.items = SideEffectsProxyDict(vt, side_effects)
-        # Saves a "proxy" dict to the original __dict__ of the object
-        # This allows track mutations on __dict__ (using side effects) without
-        # modifying the original __dict__
-        self.dict_proxy = dict_proxy
+        self.items = SideEffectsProxyDict(vt, tx)
 
     def setitem(self, name: str, value: VariableTracker) -> None:
         self.items[name] = value
 
     def getitem(self, name: str) -> VariableTracker:
-        if name in self.items:
-            return self.items[name]
-        else:
-            return self.dict_proxy[name]
+        return self.items[name]
 
     def contains(self, name: str) -> bool:
-        return name in self.items or name in self.dict_proxy
+        return name in self.items
 
     def getitem_or_default(
         self,
@@ -2078,96 +2125,6 @@ class DunderDictVariable(ConstDictVariable):
             value = default()
             self.items[name] = value
             return value
-
-    # We need to overload the three functions below:
-    # - __contains__
-    # - getitem_const
-    # - maybe_getitem_const
-    # - getitem_const_raise_exception_if_absent
-    # because the default implementation in ConstDictVariable will directly look
-    # up the name in self.items, which might add undesired guards.
-    def __contains__(self, vt: VariableTracker) -> bool:
-        name = vt.as_python_constant()
-        return self.contains(name)
-
-    def getitem_const(
-        self, tx: "InstructionTranslator", arg: VariableTracker
-    ) -> VariableTracker:
-        name = arg.as_python_constant()
-        if self.contains(name):
-            return self.getitem(name)
-        return super().getitem_const(tx, arg)
-
-    def maybe_getitem_const(self, arg: VariableTracker) -> VariableTracker | None:
-        name = arg.as_python_constant()
-        if self.contains(name):
-            return self.getitem(name)
-        return None
-
-    def getitem_const_raise_exception_if_absent(self, tx, arg):
-        name = arg.as_python_constant()
-        if self.contains(name):
-            return self.getitem(name)
-        return super().getitem_const_raise_exception_if_absent(tx, arg)
-
-    def call_method(
-        self,
-        tx: "InstructionTranslator",
-        name: str,
-        args: list[VariableTracker],
-        kwargs: dict[str, VariableTracker],
-    ) -> VariableTracker:
-        if name in ("items", "keys", "values"):
-            if args or kwargs:
-                raise_args_mismatch(
-                    tx,
-                    name,
-                    "0 args and 0 kwargs",
-                    f"{len(args)} args and {len(kwargs)} kwargs",
-                )
-            if self.source:
-                tx.output.guard_on_key_order.add(self.source)
-            merged_items = self._get_merged_dict(tx)
-            merged_dict = ConstDictVariable(merged_items, user_cls=dict)
-
-            if name == "items":
-                return DictItemsVariable(merged_dict)
-            elif name == "keys":
-                return DictKeysVariable(merged_dict)
-            elif name == "values":
-                return DictValuesVariable(merged_dict)
-        elif name == "get":
-            if len(args) not in (1, 2):
-                raise_args_mismatch(tx, name, "1 or 2 args", f"{len(args)} args")
-            name = args[0].as_python_constant()
-            if self.contains(name):
-                return self.getitem(name)
-            return CONSTANT_VARIABLE_NONE if len(args) == 1 else args[1]
-        return super().call_method(tx, name, args, kwargs)
-
-    def _get_merged_dict(
-        self, tx: "InstructionTranslator"
-    ) -> dict[VariableTracker, VariableTracker]:
-        """Get all items as a proper dict, merging dict_proxy and side effects."""
-        Hasher = ConstDictVariable._HashableTracker
-
-        def make_key(k):
-            return Hasher(VariableTracker.build(tx, k))
-
-        merged = {}
-
-        for k, v in self.dict_proxy.items():
-            merged[make_key(k)] = v
-
-        d = self.items.side_effects.store_attr_mutations.get(self.items.item, {})
-        for k, v in d.items():
-            if isinstance(v, variables.DeletedVariable):
-                key_obj = make_key(k)
-                merged.pop(key_obj, None)
-            else:
-                merged[make_key(k)] = v
-
-        return merged
 
     # Mutations to __dict__ are tracked through side effects (SideEffectsProxyDict),
     # so we don't need to install guards. Guard installation is overridden to no-op.

test/dynamo/test_dicts.py
@@ -649,6 +649,33 @@ class DictTests(torch._dynamo.test_case.TestCase):
         res = opt_fn(x)
         self.assertEqual(ref, res)
 
+    def test_len_dunder_dict_after_delete(self):
+        # After deleting an attribute during tracing, len(obj.__dict__) must
+        # reflect the deletion. The bug: SideEffectsProxyDict.__len__ returned
+        # len(item_dict) + len(side_effects_table), double-counting deleted
+        # entries and keys mutated from item_dict.
+        class UserDefined:
+            def __init__(self) -> None:
+                self.a = 3
+                self.b = 5
+
+            def run(self, x):
+                del self.a
+                # len should be 1 (only b remains), not 3 (item_dict=2 + mutations=1)
+                return x * len(self.__dict__)
+
+        obj1 = UserDefined()
+        obj2 = UserDefined()
+
+        def fn(x, obj):
+            return obj.run(x)
+
+        x = torch.ones(2)
+        ref = fn(x, obj1)
+        opt_fn = torch.compile(fn, backend="eager", fullgraph=True)
+        res = opt_fn(x, obj2)
+        self.assertEqual(ref, res)
+
     def test_contains_module_dunder_dict(self):
         class MyModule(torch.nn.Module):
             def __init__(self) -> None:

torch/_dynamo/variables/__init__.py
@@ -64,6 +64,7 @@ from .dicts import (
 )
 from .distributed import BackwardHookVariable, DistributedVariable
 from .functions import (
+    BaseUserFunctionVariable,
     BuiltinMethodVariable,
     CollectionsNamedTupleFunction,
     CreateTMADescriptorExperimentalVariable,
@@ -174,6 +175,7 @@ from .user_defined import (
     UserDefinedObjectVariable,
     UserDefinedSetVariable,
     UserDefinedTupleVariable,
+    UserDefinedVariable,
 )
 
 

torch/_dynamo/variables/functions.py
@@ -394,17 +394,7 @@ class BaseUserFunctionVariable(VariableTracker):
 
     def get_dict_vt(self, tx: "InstructionTranslator") -> "DunderDictVariable":
         if self.dict_vt is None:
-            dict_proxy: dict[str, VariableTracker] = {}
-
-            if not istype(self, NestedUserFunctionVariable):
-                fn = self.get_function()
-                dict_proxy = {
-                    name: VariableTracker.build(
-                        tx, value, source=self.source and AttrSource(self.source, name)
-                    )
-                    for name, value in fn.__dict__.items()
-                }
-            self.dict_vt = variables.DunderDictVariable.create(tx, self, dict_proxy)
+            self.dict_vt = variables.DunderDictVariable.create(tx, self)
         return self.dict_vt
 
     def call_method(
@@ -1522,6 +1512,9 @@ class LocalGeneratorFunctionVariable(BaseUserFunctionVariable):
             source=self.source,
         )
 
+    def get_real_python_backed_value(self) -> object:
+        return self.vt.get_real_python_backed_value()
+
 
 class FunctionDecoratedByContextlibContextManagerVariable(
     LocalGeneratorFunctionVariable
@@ -2453,6 +2446,9 @@ class WrapperUserFunctionVariable(BaseUserFunctionVariable):
             [self, VariableTracker.build(tx, self.attr_to_trace), *all_args],
             kwargs,
         )
+
+    def get_real_python_backed_value(self) -> object:
+        return getattr(self.wrapper_obj, self.attr_to_trace)
 
 
 class WrapperUserMethodVariable(WrapperUserFunctionVariable):

torch/_dynamo/variables/user_defined.py
@@ -1240,16 +1240,7 @@ class UserDefinedObjectVariable(UserDefinedVariable):
 
     def get_dict_vt(self, tx: "InstructionTranslator") -> "DunderDictVariable":
         if self.dict_vt is None:
-            dict_proxy = {
-                key: VariableTracker.build(
-                    tx,
-                    value,
-                    source=self.source
-                    and DictGetItemSource(AttrSource(self.source, "__dict__"), key),
-                )
-                for key, value in self.value.__dict__.items()
-            }
-            self.dict_vt = variables.DunderDictVariable.create(tx, self, dict_proxy)
+            self.dict_vt = variables.DunderDictVariable.create(tx, self)
         return self.dict_vt
 
     def is_underlying_vt_modified(self, side_effects: "SideEffects") -> bool:

commit 2e577df59ce2af13380c5f2fdc4b9abb26122111

Fix InternalTorchDynamoError on attribute access of a compiled method
torch.compile(obj.meth) stores the bound method in the wrapper's
_torchdynamo_inline attribute. When traced code reads any attribute off that
wrapper, Dynamo materializes the backing object's instance dict with
object.__getattribute__(value, "__dict__") in
SideEffectsProxyDict.get_example_value_dict, and that raises
AttributeError: 'method' object has no attribute '__dict__'. It escapes as
InternalTorchDynamoError because the graceful unimplemented() beside it only
guards the NO_SUCH_SUBOBJ branch.

Root cause: object.__getattribute__ deliberately bypasses the type's
tp_getattro. That bypass is correct for user-defined classes, where a custom
__getattribute__ must not be able to lie to Dynamo about the instance dict, and
it was generalized to every type. But a method owns no __dict__ slot at all;
m.__dict__ resolves only because method's tp_getattro forwards the lookup to
__func__, and m.__dict__ is m.__func__.__dict__ is the very same object.
Bypassing the forwarding therefore skips the only route to a dict that does
exist, and CPython's AttributeError is the correct answer to the wrong
question.

The fix takes two shapes, because two different kinds of caller reach this
pattern.

In get_example_value_dict the unit is a VariableTracker, not a raw object. For
WrapperUserFunctionVariable, get_real_python_backed_value returns the inline
target -- which is the method -- but vt.source denotes the wrapper, and
get_value___dict__ gives every key a source underneath it. Reading the target's
dict there would pair values from one object with sources naming another, and
Dynamo would then build guards for a relationship that never held. So this
reads the wrapper's own __dict__, the object vt.source actually designates. The
wrapper is a plain function and owns a real dict, so no method reaches
object.__getattribute__ on this path at all.

object_setattr_ignore_descriptor and object_delattr_ignore_descriptor are the
side-effect replay paths. They take real runtime objects rather than tracked
values, so there is no source to stay consistent with, and they follow __func__
directly. That matches eager: writing through method.__dict__ writes to the
function's dict. Those helpers already special-case threading.local for the
same underlying reason -- object.__getattribute__ cannot see its dict either.

An alternative was to unwrap inside
WrapperUserFunctionVariable.get_real_python_backed_value so it hands back
__func__ rather than the method. That was rejected: the accessor has many
callers which perform identity and issubclass checks and legitimately need the
real method object.

The blast radius is wider than the report suggests. Anything producing a method
object is affected, so classmethods are included; it is not specific to
nn.Module, nor to dynamo_ctx, nor to inspect.getfullargspec. Every attribute
name reaches the same path, because BaseUserFunctionVariable.getattro_impl
materializes __dict__ before dispatching on the name, so even __name__ trips
it. staticmethods and plain functions are unaffected, being functions that own
a __dict__.

This is a regression from #177658, which made SideEffectsProxyDict materialize

torch/_dynamo/utils.py
@@ -4431,6 +4431,10 @@ def object_has_getattribute(value: Any) -> bool:
 def object_setattr_ignore_descriptor(obj: Any, name: str, value: Any) -> None:
     # Mirror the instance-dict update path in _PyObject_GenericSetAttrWithDict:
     # https://github.com/python/cpython/blob/v3.13.0/Objects/object.c#L1679-L1741
+    if isinstance(obj, types.MethodType):
+        # A method owns no __dict__; it forwards attribute lookups to __func__,
+        # and object.__getattribute__ skips that forwarding.
+        obj = obj.__func__
     try:
         d = object.__getattribute__(obj, "__dict__")
     except AttributeError:
@@ -4447,6 +4451,8 @@ def object_setattr_ignore_descriptor(obj: Any, name: str, value: Any) -> None:
 def object_delattr_ignore_descriptor(obj: Any, name: str) -> None:
     # Same path as object_setattr_ignore_descriptor with a NULL value.
     # https://github.com/python/cpython/blob/v3.13.0/Objects/object.c#L1679-L1741
+    if isinstance(obj, types.MethodType):
+        obj = obj.__func__
     try:
         d = object.__getattribute__(obj, "__dict__")
     except AttributeError:

torch/_dynamo/variables/dicts.py
@@ -1423,6 +1423,13 @@ class SideEffectsProxyDict(collections.abc.MutableMapping[kV, VariableTracker]):
         elif isinstance(vt, variables.LocalGeneratorFunctionVariable):
             return SideEffectsProxyDict.get_example_value_dict(vt.vt)
         else:
+            if isinstance(vt, variables.WrapperUserFunctionVariable):
+                # get_real_python_backed_value returns the inline target, which
+                # may be a method and owns no __dict__ of its own. vt.source
+                # denotes the wrapper, and the keys below are given sources under
+                # it, so read the wrapper's __dict__ to keep values and sources
+                # describing the same object.
+                return object.__getattribute__(vt.wrapper_obj, "__dict__")
             value = vt.get_real_python_backed_value()
             if value is not NO_SUCH_SUBOBJ:
                 if isinstance(vt, variables.UserDefinedObjectVariable):

test/dynamo/test_repros.py
@@ -8138,7 +8138,6 @@ SavedForBackwardsAOTOutput(idx=5)""",
         # that would force a recompile when the length/signal relationship flips.
         self.assertEqual(cnt.frame_count, 1)
 
-    @unittest.expectedFailure
     def test_method_dunder_dict_setitem(self):
         # Reproducer for: getattr(obj, method_name).__dict__['key'] = value
         # method.__dict__ is handled specially by CPython at C level (no
@@ -8153,6 +8152,79 @@ SavedForBackwardsAOTOutput(idx=5)""",
         x = torch.randn(2)
         _ = fn(x)
         self.assertTrue(getattr(self, self._testMethodName).__dict__.get("slow_test"))
+
+    # https://github.com/pytorch/pytorch/issues/190171
+    @parametrize(
+        "kind",
+        ["module_method", "plain_method", "classmethod", "staticmethod", "function"],
+    )
+    def test_getattr_on_compiled_method(self, kind):
+        # torch.compile(obj.meth) stores the bound method in
+        # _torchdynamo_inline. A method owns no __dict__ and forwards lookups to
+        # __func__, so materializing its __dict__ used to raise AttributeError.
+        # staticmethod/function are controls: those are plain functions.
+        class Mod(torch.nn.Module):
+            def meth(self, x):
+                return x
+
+        class Plain:
+            def meth(self, x):
+                return x
+
+            @classmethod
+            def cls_meth(cls, x):
+                return x
+
+            @staticmethod
+            def stat(x):
+                return x
+
+        def free_fn(x):
+            return x
+
+        target = {
+            "module_method": Mod().meth,
+            "plain_method": Plain().meth,
+            "classmethod": Plain.cls_meth,
+            "staticmethod": Plain().stat,
+            "function": free_fn,
+        }[kind]
+        wrapped = torch.compile(target, backend="eager")
+
+        def fn(x):
+            return x + 1, wrapped.__name__, wrapped.__qualname__
+
+        x = torch.zeros(1)
+        expected = fn(x)
+        actual = torch.compile(fn, backend="eager", fullgraph=True)(x)
+        self.assertEqual(expected, actual)
+
+    # https://github.com/pytorch/pytorch/issues/190171
+    def test_getfullargspec_on_dynamo_ctx_method(self):
+        # What pytorch-lightning does: rebind a step method to a dynamo-wrapped
+        # version, then introspect its signature from inside the traced region.
+        traced = []
+
+        def takes_param(fn, name):
+            if hasattr(fn, "__wrapped__"):
+                fn = fn.__wrapped__
+            return name in inspect.getfullargspec(fn).args
+
+        class Model(torch.nn.Module):
+            def step(self, x, dataloader_iter=None):
+                traced.append(takes_param(self.step, "dataloader_iter"))
+                return x * 2
+
+            def forward(self, x):
+                return self.step(x)
+
+        model = Model()
+        compiled = torch.compile(model, backend="eager")
+        model.step = compiled.dynamo_ctx(model.step)
+
+        expected = takes_param(model.step, "dataloader_iter")
+        compiled(torch.randn(4))
+        self.assertEqual(traced, [expected])
 
     def test_elementwise_dtypes_constant_fold(self):
         from torch._prims_common import (